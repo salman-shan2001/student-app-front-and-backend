50ffaab6b496a71be689ba930e6ace336b1ace95
salman-shan2001 <salmanfarizy454@gmail.com>
2024-06-07 14:54:12 +0530

    updated

--- src/components/Naevebar.jsx
+++ src/components/Naevebar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Naevebar = () => {
   return (
     <div>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <nav className="navbar navbar-expand-lg bg-primary">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">FISAT</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
